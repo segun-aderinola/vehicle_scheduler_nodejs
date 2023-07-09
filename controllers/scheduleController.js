b3103a4fbc7c326eb763ddcf15c894ce29172cfe
= <=>
2023-07-09 21:42:34 +0100

    created all endpoints

--- controllers/scheduleController.js
+++ controllers/scheduleController.js
@@ -47,22 +47,154 @@ const createSchedule = async(req, res, next) =>{
 
 }
 
-const getSchedule = (req, res, next) =>{
-    res.send('Hello')
+const getSchedule = async(req, res, next) =>{
+    const scheduleId = req.params.id;
+    if(!scheduleId){
+        return res.status(422).json({
+            success: false,
+            message: 'Schedule ID is required',
+            
+        })
+    }
+    try{
+        const get_schedule = await schedule.findOne({_id: scheduleId});
+        if(get_schedule){
+            return res.status(200).json({
+                success: true,
+                message: 'Data successfully retrieved',
+                result: get_schedule
+            })
+        }
+        else{
+            return res.status(404).json({
+                success: false,
+                message: 'Unable to find schedule',
+                result: get_schedule
+            });
+        }
+    }
+    catch(err){
+        return res.status(500).json({
+            success: false,
+            message: 'Unable to fetch schedule',
+            err,
+        });
+    }
 
 }
-const getAllSchedules = (req, res, next) =>{
-    res.send('Hello')
+const getAllSchedules = async(req, res, next) =>{
+    try{
+        const get_schedule = await schedule.find();
+        if(get_schedule){
+            if(get_schedule.length === 0){
+                return res.status(403).json({
+                    success: true,
+                    message: 'No Data Available',                
+                })
+            }
+            else{
+            return res.status(200).json({
+                success: true,
+                message: 'Data successfully retrieved',
+                result: get_schedule
+            })
+        }
+        }
+        else{
+            return res.status(404).json({
+                success: false,
+                message: 'Unable to find schedule',
+                result: get_schedule
+            });
+        }
+    }
+    catch(err){
+        return res.status(500).json({
+            success: false,
+            message: 'Unable to fetch schedule',
+            err,
+        });
+    }
+    
 
 }
 
-const updateSchedule = (req, res, next) =>{
-    res.send('Hello')
+const updateSchedule = async(req, res, next) =>{
+    const id = req.params.id.toString();
+    const scheduleData = req.body;    
+    
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(422).json({
+            success: false,
+            Message: 'Invalid Data sent',
+            error: errors.array(),
+        })
+    }
+    
+
+    try{
+      const update =  await schedule.updateOne({_id: id},  scheduleData);
+      if(update){
+            return res.status(200).json({
+                success: true,
+                Message: 'Schedule updated successfully',                              
+            })
+        }else{
+            return res.status(401).json({
+                success: false,
+                message: 'Unable to update Schedule',
+                error: 'Error saving data:'+err,
+                
+            })
+        }
+    }catch(err){
+        return res.status(500).json({
+            success: false,
+            message: 'Unable to send request',
+            error: err
+        })
+    }
 
 }
 
-const deleteSchedule = (req, res, next) =>{
-    res.send('Hello')
+const deleteSchedule = async (req, res, next) =>{
+    const id = req.params.id;
+
+    try{
+        const findUser = await schedule.findOne({_id:id});
+        if(!findUser){
+            return res.status(404).json({
+                success: false,
+                message: 'Unable to find Schedule',
+                
+            });
+        }
+        else{
+        const del = await schedule.deleteOne({_id: id});
+
+        if(del){
+            return res.status(200).json({
+                success: true,
+                message: 'Schedule successfully deleted',
+                
+            })
+        }
+        else{
+            return res.status(401).json({
+                success: false,
+                message: 'Unable to delete schedule',                
+            });
+        }
+    }
+    }   
+    catch(err){
+        return res.status(500).json({
+            success: false,
+            message: 'Unable to send request',
+            error: err
+        })
+    }
 
 }
 
